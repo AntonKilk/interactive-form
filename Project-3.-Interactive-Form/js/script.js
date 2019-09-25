248889f08219033adc34a1c779a144c2d90585d8
AntonKilk <ant.kilk@gmail.com>
2019-09-25 17:56:04 +0300

    1
    1

--- Project-3.-Interactive-Form/js/script.js
+++ Project-3.-Interactive-Form/js/script.js
@@ -1,6 +1,6 @@
 // Hides the "Other" job role input 
 $('#other-title').hide();
 //Reveals the "Other" job role input if "other" option chosen
-if ($('#title').text() === "other"){
-console.log('test');
+if ($('#title option:selected').text() === "Other"){
+    $('#other-title').show();
 }
